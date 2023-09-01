bc01477dfe4078470950f0528f2a66fcd7ba54cb
Ankit Litouriya <118179332+Ankitlitouriya@users.noreply.github.com>
2023-09-01 20:34:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+window.addEventListner('resize',displayWindowSize);
+
+ function displayWindowSize() {
+ 	let widht = window.innerWidth;
+	 let height = window.innerHeight;
+	 let sizeInfo = document.getElementById('sizeInfo');
+    sizeInfo.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+ }
+
+displayWindowSize();
